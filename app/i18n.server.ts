b561d9c0ab1a5812fdc6ea63b66e76966fbc7f48
airnan <hernantorrisi@gmail.com>
2022-04-06 23:28:57 -0400

    path locale test

--- app/i18n.server.ts
+++ app/i18n.server.ts
@@ -3,9 +3,17 @@ import path from 'path';
 
 // You will need to provide a backend to load your translations, here we use the
 // file system one and tell it where to find the translations.
-const backend = new FileSystemBackend(path.resolve('./public/locales'));
+
+console.log('=======================');
+console.log(__dirname);
+const localesPath = process.env.NODE_ENV === 'development' ? './public/locales' : './locales';
+
+const backend = new FileSystemBackend(path.resolve(localesPath));
 
 export const i18n = new RemixI18Next(backend, {
   fallbackLng: 'en', // here configure your default (fallback) language
   supportedLanguages: ['es', 'en'], // here configure your supported languages
+  i18nextOptions: {
+
+  }
 });
